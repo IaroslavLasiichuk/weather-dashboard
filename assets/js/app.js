c3eeed8d92d05f243abb514e26ff6494375ebfb3
IaroslavLasiichuk <lasmant@yahoo.com>
2023-02-04 10:15:09 -0700

    HTTP

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ const dayFive = document.querySelector('.day-5');
 // // Get the latitude and longitude of the city
     function getLatitudeAndLongitude() {
         let cityId = document.querySelector('#tags').value;
-        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityId} &appid=843f058e4044327330ceeb93cf7b413a`)
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityId} &appid=843f058e4044327330ceeb93cf7b413a`)
             .then(function (response) {
                 return response.json();
             })
@@ -35,12 +35,12 @@ function getWeather(city) {
     let cityId = document.querySelector('#tags').value;
     let getLatitude = localStorage.getItem(`${cityId} lat`);
     let getLongitude = localStorage.getItem(`${cityId} lon`);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${getLatitude}&lon=${getLongitude}&appid=843f058e4044327330ceeb93cf7b413a&units=imperial`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${getLatitude}&lon=${getLongitude}&appid=843f058e4044327330ceeb93cf7b413a&units=imperial`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            showIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+            showIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
             cityName.textContent = `${data.name} ${dayjs().format('MM/DD/YYYY')}`;
             showTemp.textContent = `${'Temp:'} ${data.main.temp}`;
             showHumidity.textContent = `${'Humidity:'} ${data.main.humidity}`;
@@ -52,7 +52,7 @@ function getWeather(city) {
             console.log(localStorage.getItem('name'));
             let setNameToLocalStorage = localStorage.setItem('name', data.name)
             list.textContent = localStorage.getItem('name');
-            fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${getLatitude}&lon=${getLongitude}&appid=843f058e4044327330ceeb93cf7b413a&units=imperial`)
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${getLatitude}&lon=${getLongitude}&appid=843f058e4044327330ceeb93cf7b413a&units=imperial`)
                 .then(function (response) {
                     return response.json()
                         .then(function (data) {
@@ -67,31 +67,31 @@ function getWeather(city) {
                              
                                 dayOne.innerHTML =
                                     `<p>${dayjs().add(1, 'day').format('MM/DD/YYYY')}</p>
-                    <img src="https://openweathermap.org/img/wn/${array[0].weather[0].icon}.png" alt=""> 
+                    <img src="http://openweathermap.org/img/wn/${array[0].weather[0].icon}.png" alt=""> 
                     <p> ${'Temp:'} ${array[0].main.temp} <p>
                     <p> ${'Humidity:'} ${array[0].main.humidity}</p>
                     <p>  ${'Wind:'} ${array[0].wind.speed}</p>`;
                                 dayTwo.innerHTML =
                                     `<p>${dayjs().add(2, 'day').format('MM/DD/YYYY')}</p>
-                                    <img src="https://openweathermap.org/img/wn/${array[1].weather[0].icon}.png" alt=""> 
+                                    <img src="http://openweathermap.org/img/wn/${array[1].weather[0].icon}.png" alt=""> 
                     <p> ${'Temp:'} ${array[1].main.temp} <p>
                     <p> ${'Humidity:'} ${array[1].main.humidity}</p>
                     <p>  ${'Wind:'} ${array[1].wind.speed}</p>`;
                                 dayThree.innerHTML =
                                     `<p>${dayjs().add(3, 'day').format('MM/DD/YYYY')}</p>
-                                    <img src="https://openweathermap.org/img/wn/${array[2].weather[0].icon}.png" alt=""> 
+                                    <img src="http://openweathermap.org/img/wn/${array[2].weather[0].icon}.png" alt=""> 
                     <p> ${'Temp:'} ${array[2].main.temp} <p>
                     <p> ${'Humidity:'} ${array[2].main.humidity}</p>
                     <p>  ${'Wind:'} ${array[2].wind.speed}</p>`;
                                 dayFour.innerHTML =
                                     `<p>${dayjs().add(4, 'day').format('MM/DD/YYYY')}</p>
-                                    <img src="https://openweathermap.org/img/wn/${array[3].weather[0].icon}.png" alt=""> 
+                                    <img src="http://openweathermap.org/img/wn/${array[3].weather[0].icon}.png" alt=""> 
                     <p> ${'Temp:'} ${array[3].main.temp} <p>
                     <p> ${'Humidity:'} ${array[3].main.humidity}</p>
                     <p> ${'Wind:'} ${array[3].wind.speed}</p>`;
                                 dayFive.innerHTML =
                                     `<p>${dayjs().add(5, 'day').format('MM/DD/YYYY')}</p>
-                                    <img src="https://openweathermap.org/img/wn/${array[4].weather[0].icon}.png" alt=""> 
+                                    <img src="http://openweathermap.org/img/wn/${array[4].weather[0].icon}.png" alt=""> 
                     <p> ${'Temp:'} ${array[4].main.temp} <p>
                     <p> ${'Humidity:'} ${array[4].main.humidity}</p>
                     <p>  ${'Wind:'} ${array[4].wind.speed}</p>`;
